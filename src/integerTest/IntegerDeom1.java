7ac5a3c38a0afa8d636bda841c39aea924feece9
liuhang <liuhang93@foxmail.com>
2016-12-09 10:14:02 +0800

    Integer测试,包装类

---
+++ src/integerTest/IntegerDeom1.java
@@ -0,0 +1,16 @@
+package integerTest;
+
+/**
+ * Created by liuhang on 2016/12/5.
+ */
+public class IntegerDeom1 {
+    public static void main(String[]args){
+        System.out.println(Integer.toBinaryString(-60));
+        System.out.println(Integer.toOctalString(60)); //八进制
+        System.out.println(Integer.toHexString(60)); //十六进制
+        System.out.println(Integer.MAX_VALUE);// int类型最大的数 4个字节 2^31-1
+        System.out.println(Integer.MIN_VALUE); //int类型最小的数 -2^31
+        Integer a=new Integer("23");
+        Integer b=new Integer(211);
+    }
+}
